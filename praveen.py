e82918c82a44f4feb3ed8235e1561f12dad1902e
Praveenm2623 <136802636+Praveenm2623@users.noreply.github.com>
2023-07-28 15:53:52 +0530

    Update praveen.py

--- praveen.py
+++ praveen.py
@@ -36,7 +36,75 @@ def fn(n):
  return fn(n-1) + fn(n-2)
 
 num = int(input("Enter a number : "))
+
+
 if num > 0:
  print("fn(", num, ") = ",fn(num) , sep ="")
 else:
  print("Error in input")
+
+#Base Conversion
+def bin2Dec(val):
+ rev=val[::-1]
+ dec = 0
+ i = 0
+ for dig in rev:
+ dec += int(dig) * 2**i
+ i += 1
+
+ return dec
+def oct2Hex(val):
+ rev=val[::-1]
+ dec = 0
+ i = 0
+ for dig in rev:
+ dec += int(dig) * 8**i
+ i += 1
+ list=[]
+ while dec != 0:
+ list.append(dec%16)
+ dec = dec // 16
+ nl=[]
+ for elem in list[::-1]:
+ if elem <= 9:
+ nl.append(str(elem))
+ else:
+ nl.append(chr(ord('A') + (elem -10)))
+ hex = "".join(nl)
+
+ return hex
+num1 = input("Enter a binary number : ")
+print(bin2Dec(num1))
+num2 = input("Enter a octal number : ")
+print(oct2Hex(num2))
+
+sentence = input("Enter a sentence : ")
+wordList = sentence.split(" ")
+print("This sentence has", len(wordList), "words")
+digCnt = upCnt = loCnt = 0
+for ch in sentence:
+ if '0' <= ch <= '9':
+ digCnt += 1
+ elif 'A' <= ch <= 'Z':
+ upCnt += 1
+ elif 'a' <= ch <= 'z':
+ loCnt += 1
+print("This sentence has", digCnt, "digits", upCnt, "upper case letters", loCnt,
+"lower case letters")
+
+str1 = input("Enter String 1 \n")
+str2 = input("Enter String 2 \n")
+if len(str2) < len(str1):
+ short = len(str2)
+ long = len(str1)
+else:
+ short = len(str1)
+ long = len(str2)
+
+matchCnt = 0
+for i in range(short):
+ if str1[i] == str2[i]:
+ matchCnt += 1
+print("Similarity between two said strings:")
+print(matchCnt/long)
+
